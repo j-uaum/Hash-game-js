386fd5b1acf42603dda09a5430d9fe42ed36548c
juaum <jotinhagamer4@gmail.com>
2022-11-23 00:19:17 -0300

    ex

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
-let board = ['','','','','','','','','']
-let playerTime = 0;
-let symbols = ['o', 'x']
-let gameOver = false;
+let board = ['','','','','','','','',''] //para representar o tabuleio no js
+let playerTime = 0; // esse define as vezes dos jogadores
+let symbols = ['o', 'x'] //os simbolos que simbolizam os jogadores
+let gameOver = false; // determina se o jogo acabou ou nao, quando falso. segue o jogo
 let winStates = [
     [0,1,2],
-    [3,4,5],
+    [3,4,5], // essas são todas as posibilidades de vitorias previstas no jogo
     [6,7,8],
     [0,3,6],
     [1,4,7],
@@ -16,21 +16,21 @@ let winStates = [
 
 
 
-function handleMove (position){
+function handleMove (position){  // a função que da vida ao jogo, ela recebe as posições la da interface quando ocorre a chamada dessa func
 
-        if(gameOver){
+        if(gameOver){ // ele gameOver = false, segue o jogo, se nao bloqueia
             return;
         }
 
-                if(board[position] == ''){
+                if(board[position] == ''){ // verificando se o board esta vazio, para nao ficar trocando os simbolos
                     
                     board[position] = symbols[playerTime];
 
-                    gameOver = isWin();
+                    gameOver = isWin(); //toda vez que acontecer uma jogada, precisamos verificar se tem um vencedor
 
     
 /* playerTime = (playerTime == 0 )?1 : 0;  */ 
-                        if(!gameOver){
+                        if(!gameOver){ // essa veficação é sobre, o jogo só vai trocar de jogador quando for gameOver == false, senao nao troca
 
                             if(playerTime == 0){
                             playerTime = 1
@@ -39,11 +39,12 @@ function handleMove (position){
                                 }
     }
 }
-            return gameOver;
+            return gameOver; // retorna o novo valor de gameOver definido com isWin()
 }
 
 function isWin(){
 
+// essa é toda a parte da logica do jogo da velha
 
                 for(let i = 0; i < winStates.length; i++){
                     let seq = winStates[i];
@@ -55,7 +56,7 @@ function isWin(){
                                 return true;
                             }
                     
-
+// caso tudo isso bater como esta declarado, ele retorna um true, bloqueando o jogo
                 }
         
     return false;
--- js/inteface.js
+++ js/inteface.js
@@ -2,43 +2,41 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 let squares = document.querySelectorAll('.square');
 
-squares.forEach((square)=>{
+squares.forEach((square)=>{ // faz uma varredura de array 
 
-    square.addEventListener('click', handleClick);
+    square.addEventListener('click', handleClick); // para cada quadrado, faz um handleClick
 })
 //
 
 })
 
-function handleClick(event){
+function handleClick(event){ // recebe evento
     console.log(event.target)
 
-    let square = event.target;
-    let position = square.id;
+    let square = event.target; // retorna local clicado
+    let position = square.id; // retorna id do local clicado
    
-  if(handleMove(position)) {
-    
-    setTimeout(()=>{alert('O jogo acabou, o vencedor foi' + playerTime)}, 10)
+  if(handleMove(position)) { //chamada do handleMove levando o position, se o handleMove for verdadeiro é pq teve gameover
+    // pq, como estamos retornando o gameOver la no game, o hadlemove tem valor gameover true tbm
+    setTimeout(()=>{alert('O jogo acabou, o vencedor foi' + playerTime)}, 10) // criado um intevalo para o alert
     
   };
-   updateSquares();  
+   updateSquares();  // depois do clique ele precisa ser atualizado na tela
 };
 //
 
-
-
 function updateSquares(){
     let squares = document.querySelectorAll('.square');
-
+//re define squares
 
     squares.forEach((square)=>{
 
        let position = square.id; 
-        let symbol = board[position]
+        let symbol = board[position] // pega a posição aonde o codigo colocou o x ou o do jogo
         
 
-        if(symbol != ''){
-            square.innerHTML = `<div class='${symbol}'></div>`
+        if(symbol != ''){ // se o board position for diferente de vazio, ele imprime
+            square.innerHTML = `<div class='${symbol}'></div>` // imprime uma classe com o symbolo
         }
 
 
